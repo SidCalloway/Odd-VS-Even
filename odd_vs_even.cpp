f5244ad9b0cef465d266c1ca7e617df9dc0983a9
Sidney Calloway Jr <sid@lovefateventures.com>
2019-10-10 04:44:12 -0700

    Add files via upload

---
+++ odd_vs_even.cpp
@@ -0,0 +1,31 @@
+// October 9th 2019 //
+// 2nd Day of Writing Programs //
+// Project 00003 | 'Odd VS Even' //
+// Language: C++ //
+// Blessed Sid Build //
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+int no;
+cout << "Enter Your Number: ";
+cin >> no;
+if(no%2==0)
+{
+	cout<<"Even";
+
+}
+else
+{
+	cout<<"Odd";
+}
+return 0;
+
+}
+
+//---Respectfully
+//---Sid Calloway---//
